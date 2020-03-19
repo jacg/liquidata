c96880c7479e3aca0753a3c72d9e9a2f40b6a44c
Jacek Generowicz <github@my-post-office.net>
2020-03-19 01:45:36 +0100

    Change operator type matrices
    In this scheme, `-` is supposed to be the default sequencing operation. It
    should be able to connect
    
    + source - pipe   ->  source
    + pipe   - pipe   ->  pipe
    + pipe   - sink   ->  sink
    + source - sink   ->  ready
    
    `-` should convert callables into pipes that map the callable over the stream:
    
    + source - func   ->  source
    + pipe   - func   ->  pipe
    + func   - pipe   ->  pipe
    + func   - sink   ->  sink
    
    To use callables in filters rather than maps, use `+` instead of `-`. This
    should work in two situations:
    
    + source + func   ->  source
    + pipe   + func   ->  pipe
    
    To use callables in sinks rather than pipes, use `>>` instead of `-`. This
    should also work in two situations:
    
    + source >> func  ->  ready
    + pipe   >> func  ->  sink
    
    Additionally `>>` is also being expected to work in two more situations:
    
    + source >> sink  ->  ready
    + func   >> sink  ->  sink
    
    but these could be suppressed with no loss of generality.

--- typedflow.py
+++ typedflow.py
@@ -1,18 +1,33 @@
 
 class source:
 
-    def __rshift__(self, other):
-        if isinstance(other, (source)):
-            raise TypeError
+    # TODO: reimplement all of these operators with multimethods
+    def __sub__(self, other):
+        if isinstance(other, pipe):
+            return self
         if isinstance(other, sink):
             return ready()
+        if isinstance(other, source):
+            raise TypeError
         return self
+
+    def __add__(self, other):
+        if isinstance(other, (source, pipe, sink)):
+            raise TypeError
+        return self
+
+    def __rshift__(self, other):
+        if isinstance(other, (source, pipe)):
+            raise TypeError
+        return ready()
 
 class pipe:
 
     def __sub__(self, other):
-        if isinstance(other, (source, sink)):
+        if isinstance(other, source):
             raise TypeError
+        if isinstance(other, sink):
+            return other
         return self
 
     __rsub__ = __sub__
@@ -20,9 +35,7 @@ class pipe:
     def __rshift__(self, other):
         if isinstance(other, (source, pipe, sink)):
             raise TypeError
-        return self
-
-    __rrshift__ = __rshift__
+        return sink()
 
     def __add__(self, other):
         if isinstance(other, (source, pipe, sink)):
@@ -31,10 +44,16 @@ class pipe:
 
 class sink:
 
-    def __rrshift__(self, other):
-        if isinstance(other, (source, pipe, sink)):
-            raise TypeError
+    def __sub__(self, _):
+        raise TypeError
+
+    def __rsub__(self, other):
         return self
+
+    def __rrshift__(self, other):
+        if isinstance(other, (pipe, sink)):
+            raise TypeError
+        return sink()
 
 class ready:
     pass
--- typedflow_test.py
+++ typedflow_test.py
@@ -7,29 +7,35 @@ parametrize = mark.parametrize
 func = type(lambda: 1)
 X = TypeError
 
-RHS =                           (tf.source, tf.pipe  , tf.sink  ,   func   )
+# Using the symbols rather than the functions directly makes the table and the
+# test-suite reports easier to read.
+ops = {'-': sub, '+': add, '>>': rshift}
 
-table = {sub    : { tf.source : (    X    ,     X    ,     X    ,     X    ),
-                    tf.pipe   : (    X    , tf.pipe  ,     X    , tf.pipe  ),
-                    tf.sink   : (    X    ,     X    ,     X    ,     X    ),
-                    func      : (    X    , tf.pipe  ,     X    ,     X    )},
+RHS =                         (tf.source, tf.pipe  , tf.sink  ,   func   )
 
-         rshift : { tf.source : (    X    , tf.source, tf.ready , tf.source),
-                    tf.pipe   : (    X    ,     X    ,     X    , tf.pipe  ),
-                    tf.sink   : (    X    ,     X    ,     X    ,     X    ),
-                    func      : (    X    , tf.pipe  , tf.sink  ,     X    )},
+table = {'-'  : { tf.source : (    X    , tf.source, tf.ready , tf.source),
+                  tf.pipe   : (    X    , tf.pipe  , tf.sink  , tf.pipe  ),
+                  tf.sink   : (    X    ,     X    ,     X    ,     X    ),
+                  func      : (    X    , tf.pipe  , tf.sink  ,     X    )},
 
-         add    : { tf.source : (    X    ,     X    ,     X    ,     X    ),
-                    tf.pipe   : (    X    ,     X    ,     X    , tf.pipe  ),
-                    tf.sink   : (    X    ,     X    ,     X    ,     X    ),
-                    func      : (    X    ,     X    ,     X    ,     X    )}}
+         '+'  : { tf.source : (    X    ,     X    ,     X    , tf.source),
+                  tf.pipe   : (    X    ,     X    ,     X    , tf.pipe  ),
+                  tf.sink   : (    X    ,     X    ,     X    ,     X    ),
+                  func      : (    X    ,     X    ,     X    ,     X    )},
 
-@parametrize('LHS_type, op, RHS_type, result',
-             [ (lhs, op, rhs, result)
-               for (op, sub_table) in table.items()
+         '>>' : { tf.source : (    X    ,     X    , tf.ready , tf.ready ),
+                  tf.pipe   : (    X    ,     X    ,     X    , tf.sink  ),
+                  tf.sink   : (    X    ,     X    ,     X    ,     X    ),
+                  func      : (    X    ,     X    , tf.sink  ,     X    )},
+
+        }
+
+@parametrize('LHS_type, op_symbol, RHS_type, result',
+             [ (lhs, op_symbol, rhs, result)
+               for (op_symbol, sub_table) in table.items()
                for (lhs, results)  in sub_table.items()
                for (rhs, result)   in zip(RHS, results) ])
-def test_operator_type_matrices(LHS_type, op, RHS_type, result):
+def test_operator_type_matrices(LHS_type, op_symbol, RHS_type, result):
     sample_instance = { tf.source : tf.source(),
                         tf.pipe   : tf.pipe()  ,
                         tf.sink   : tf.sink()  ,
@@ -37,6 +43,7 @@ def test_operator_type_matrices(LHS_type, op, RHS_type, result):
 
     lhs = sample_instance[LHS_type]
     rhs = sample_instance[RHS_type]
+    op  = ops[op_symbol]
 
     if result is TypeError:
 
