cf992e9732a10fd7ccbd5b2af8c6295a17d0b4a3
Jacek Generowicz <github@my-post-office.net>
2020-03-22 15:48:18 +0100

    Add guinea-pig functions for use in tests

--- reboot_test.py
+++ reboot_test.py
@@ -7,3 +7,23 @@ def test_trivial():
     net = Network(data, Sink(result.append))
     net()
     assert result == data
+
+
+###################################################################
+# Guinea pig functions for use in graphs constructed in the tests #
+###################################################################
+
+def symbolic_apply(f ): return lambda x   : f'{f}({x})'
+def symbolic_binop(op): return lambda l, r: f'({l} {op} {r})'
+def symbolic_functions(names): return map(symbolic_apply, names)
+sym_add = symbolic_binop('+')
+sym_mul = symbolic_binop('*')
+
+def square(n): return n * n
+def mulN(N): return lambda x: x * N
+def addN(N): return lambda x: x + N
+def  gtN(N): return lambda x: x > N
+def  ltN(N): return lambda x: x < N
+
+def odd (n): return n % 2 != 0
+def even(n): return n % 2 == 0
