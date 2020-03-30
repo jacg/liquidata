0b50ad04579d055a2c8727a8ad6d0289a079d55c
Jacek Generowicz <github@my-post-office.net>
2020-03-30 11:23:30 +0200

    Move guinea-pig functions to front of test suite

--- reboot_test.py
+++ reboot_test.py
@@ -17,6 +17,26 @@ from hypothesis.strategies import integers
 from hypothesis.strategies import none
 from hypothesis.strategies import one_of, sampled_from
 
+###################################################################
+# Guinea pig functions for use in graphs constructed in the tests #
+###################################################################
+
+def symbolic_apply(f ): return lambda x   : f'{f}({x})'
+def symbolic_binop(op): return lambda l, r: f'({l} {op} {r})'
+def symbolic_functions(names): return map(symbolic_apply, names)
+sym_add = symbolic_binop('+')
+sym_mul = symbolic_binop('*')
+
+def square(n): return n * n
+def mulN(N): return lambda x: x * N
+def addN(N): return lambda x: x + N
+def  gtN(N): return lambda x: x > N
+def  ltN(N): return lambda x: x < N
+
+def odd (n): return n % 2 != 0
+def even(n): return n % 2 == 0
+
+###################################################################
 
 def test_trivial():
     from reboot import flow
@@ -562,22 +582,3 @@ def test_while():
     expected = ''.join(it.takewhile(_ != 'X', data))
     got      = ''.join(flow(while_ (_ != 'X'), out)(data))
     assert got == expected
-
-###################################################################
-# Guinea pig functions for use in graphs constructed in the tests #
-###################################################################
-
-def symbolic_apply(f ): return lambda x   : f'{f}({x})'
-def symbolic_binop(op): return lambda l, r: f'({l} {op} {r})'
-def symbolic_functions(names): return map(symbolic_apply, names)
-sym_add = symbolic_binop('+')
-sym_mul = symbolic_binop('*')
-
-def square(n): return n * n
-def mulN(N): return lambda x: x * N
-def addN(N): return lambda x: x + N
-def  gtN(N): return lambda x: x > N
-def  ltN(N): return lambda x: x < N
-
-def odd (n): return n % 2 != 0
-def even(n): return n % 2 == 0
