d76e81a6a071315bd50862eeab7e29f1dbc2c5f8
Jacek Generowicz <github@my-post-office.net>
2020-09-08 10:45:18 +0200

    Add CLI control for timings

--- space_problem.py
+++ space_problem.py
@@ -2,39 +2,82 @@
 # treating `min`, `max` and `sum` as black boxes.
 
 def testit(implementation, N):
-    from time import sleep
-    def slowrange(t,N):
+    assert implementation(RANGE(N), MIN, max, sum) == (0, N-1, N*(N-1)//2)
+
+# Slowed down versions of range and min, simulating sources and consumers which
+# take non-trivial time. The CLI provides options for using these instead of
+# the real range and max in the timings.
+from time import sleep
+
+def slowrange(t):
+    def slowrange(N):
         for x in range(N):
             sleep(t)
             yield x
-    def slowmin(T):
-        from sys import maxsize
-        lo = maxsize
-        def slowmin(iterable):
-            nonlocal lo
-            for item in iterable:
-                sleep(T)
-                if item < lo:
-                    lo = item
-            return lo
-        return slowmin
+    return slowrange
 
-    #assert implementation(range(N), max, sum, slowmin(0.001)) == (N-1, N*(N-1)//2, 0)
-    #assert implementation(slowrange(0.001,N), min, max, sum) == (0, N-1, N*(N-1)//2)
-    assert implementation(range(N), min, max, sum) == (0, N-1, N*(N-1)//2)
+def slowmin(T):
+    from sys import maxsize
+    lo = maxsize
+    def slowmin(iterable):
+        nonlocal lo
+        for item in iterable:
+            sleep(T)
+            if item < lo:
+                lo = item
+        return lo
+    return slowmin
 
-# Discussion
+# Timing utility
+def time(thunk):
+    from time import time
+    start = time()
+    thunk()
+    stop  = time()
+    return stop - start
+
+# ========= Aside: Deal with CLI arguments =====================================
+from sys import argv
+usage_message = f"""
+ERROR: problem on the command line.
+Try one of:
+{argv[0]} 100000
+{argv[0]} 1000 --slowrange 0.0001
+{argv[0]} 1000 --slowmin   0.0001
+"""
+
+if len(argv) not in (2, 4): raise SystemExit(usage_message)
+N = int(argv[1])
+
+# Default values, may be overridden on CLI using --slowrange or --slowmin
+RANGE, MIN = range, min
+
+if len(argv) == 4:
+    slow, delay = argv[2:4]
+    if slow not in '--slowrange --slowmin'.split():
+        raise SystemExit(f'Unknown option: {slow}' + usage_message)
+    try:
+        delay = float(delay)
+    except ValueError:
+        raise SystemExit(f'Delay must parse to float but got: {delay}')
+    if slow == '--slowrange': RANGE = slowrange(delay)
+    if slow == '--slowmin'  : MIN   = slowmin  (delay)
+
+# ====================== Discussion =============================================
 
 # We love iterators because they allow us to process streams of data lazily,
-# allowing the processing of huge amounts of data in CONSTANT SPACE.
+# allowing the processing of huge amounts of data in CONSTANT SPACE:
 
 def source_summary(source, summary):
     return summary(source)
 
-N = 1 #10 ** 8
-print(source_summary(range(N), min))
-print(source_summary(range(N), max))
-print(source_summary(range(N), sum))
+from time import time as tick
+start = tick()
+source_summary(RANGE(N), MIN)
+source_summary(RANGE(N), max)
+source_summary(RANGE(N), sum)
+stop = tick()
+print(f'3 traversals: {N} in {stop - start:6.1f}')
 
 # Each line took a few seconds to execute, but used very little memory.
 # However, It did require 3 separate traversals of the source. So this will not
@@ -48,11 +91,9 @@ def source_summaries_TEE(source, *summaries):
     return tuple(map(source_summary, tee(source, len(summaries)),
                                      summaries))
 
-from time import time
-start = time()
-testit(source_summaries_TEE, N)
-stop = time()
-print(f'OK: {N} in {stop - start}')
+t = time(lambda: testit(source_summaries_TEE, N))
+print(f'tee   : {N} in {t:5.1f} s')
+
 # ... but the space usage goes up from `O(1)` to `O(N)`.
 
 # How can you obtain the results in a single traversal with constant memory?
@@ -90,8 +131,8 @@ class Link:
 
     def stop(self):
         self.queue.put(FINISHED)
-        print(f'Queue space: {self.maxsize} ({self.maxsize / self.count} N)  after {self.maxwhen / self.count * 100:6.2f} % items')
-        #print(f'Maximum queue size {self.maxsize} after {self.maxwhen} out of {self.count} ({self.maxwhen / self.count * 100:5.2f} %)')
+        #print(f'Queue space: {self.maxsize} ({self.maxsize / self.count} N)  after {self.maxwhen / self.count * 100:6.2f} % items')
+        print(f'Maximum queue size {self.maxsize} after {self.maxwhen} out of {self.count} ({self.maxwhen / self.count * 100:5.2f} %)')
 
     def consumer_not_listening_any_more(self):
         self.__class__ = ClosedLink
@@ -135,13 +176,8 @@ def source_summaries_PREEMPTIVE_THREAD(source, *consumers):
 
     return tuple(f.result() for f in futures)
 
-def time(thunk):
-    from time import time
-    start = time()
-    thunk()
-    stop  = time()
-    return stop - start
-
+t = time(lambda: testit(source_summaries_PREEMPTIVE_THREAD, N))
+print(f'thread: {N} in {t:5.1f} s')
 ######################################################################
 def source_summaries_FOLD(source, *consumers):
 
@@ -151,33 +187,29 @@ def source_summaries_FOLD(source, *consumers):
     fn   = { min: min,     max:  max    , sum: add}
     init = { min: maxsize, max: -maxsize, sum: 0}
 
-    accumulator =  list(map(init.get, consumers))
-    fn          = tuple(map(fn  .get, consumers))
+    # accumulator =  list(map(init.get, consumers))
+    # fn          = tuple(map(fn  .get, consumers))
+
+    # Need special case to cater for slowmin
+    accumulator =  list(init.get(c, maxsize) for c in consumers)
+    fn          = tuple(fn  .get(c, min    ) for c in consumers)
 
     for item in source:
         for i in range(len(consumers)):
             accumulator[i] = fn[i](accumulator[i], item)
     return tuple(accumulator)
-######################################################################
-
-N = 10 ** 7
-t = time(lambda: testit(source_summaries_TEE, N))
-print(f'tee   : {N} in {t:5.1f} s')
-
-# t = time(lambda: testit(source_summaries_PREEMPTIVE_THREAD, N))
-# print(f'thread: {N} in {t:5.1f} s')
 
 t = time(lambda: testit(source_summaries_FOLD, N))
 print(f'fold  : {N} in {t:5.1f} s')
+######################################################################
 
 
-
-def testitR(implementation, N):
+def testit_more_consumers(implementation, N):
     consumers = known_consumers
     assert implementation(range(N), *consumers) == tuple(c(range(N)) for c in consumers)
 
-def testit(implementation, N):
-    assert implementation(range(N), min, max, sum) == (0, N-1, N*(N-1)//2)
+# def testit(implementation, N):
+#     assert implementation(range(N), min, max, sum) == (0, N-1, N*(N-1)//2)
 
 def Bobs_classify(iterable):
     small = medium = big = 0
@@ -197,5 +229,6 @@ def exceeds_10(iterable):
 from collections import Counter
 known_consumers = [tuple, list, set, dict, sorted, min, max, sum, ','.join, Counter, enumerate, lambda x: map(lambda x:x+1, x)]
 known_consumers = [exceeds_10, sum, min, max, tuple, list, set, Counter, Bobs_classify, exceeds_10]
-testitR(source_summaries_PREEMPTIVE_THREAD, 1000)
-print('OK')
+#t = time(lambda: testit               (source_summaries_PREEMPTIVE_THREAD, N))
+#t = time(lambda: testit_more_consumers(source_summaries_PREEMPTIVE_THREAD, N))
+#print(f'link  : {N} in {t:5.1f} s')
